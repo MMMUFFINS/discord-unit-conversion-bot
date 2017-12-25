0edccbd769d46cc3925e32bf53bfbb0ec04a385d
MMMUFFINS <muffins.ruinedmylife@gmail.com>
2017-12-25 14:33:55 +0000

    moved stuff to src

---
+++ src/index.js
@@ -0,0 +1,47 @@
+/*
+  A ping pong bot, whenever you send "ping", it replies "pong".
+*/
+
+'use strict';
+
+// Import the discord.js module
+const Discord = require('discord.js');
+const fs = require('fs');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+// The token of your bot - https://discordapp.com/developers/applications/me
+const rawSecrets = fs.readFileSync('./secrets.json');
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+    // If the message is "ping"
+    if (message.content === 'ping') {
+        // Send "pong" to the same channel
+        message.channel.send('pong');
+    }
+});
+
+new Promise ((resolve, reject) => {    
+    fs.readFile('./secrets.json', (err, data) => {
+        if (err) return reject(err);
+        
+        console.log('read secret');
+        return resolve(JSON.parse(data));
+    });
+})
+.then((secrets) => {
+    let token = secrets.token;
+    client.login(token);
+})
+.catch((err) => {
+    console.error('Error!');
+    console.error(err.message);
+})
